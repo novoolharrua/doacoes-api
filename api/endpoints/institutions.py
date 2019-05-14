cf6bfa77d4c595db6f9a59cfd6e4b6a58031022f
Lucas Ferreira <ferreira.alves@live.com>
2019-05-14 00:18:42 -0300

    institution changes

--- api/endpoints/institutions.py
+++ api/endpoints/institutions.py
@@ -16,6 +16,14 @@ def endpoints_exception(code, msg):
     abort(make_response(jsonify(message=msg), code))
 
 
+status_enum = {
+    "0": 'NEW',
+    "1": 'PENDING',
+    "2": 'ACCEPTED',
+    "3": 'REFUSED',
+}
+
+
 def to_dict(institution):
     dict_format = {}
     dict_format['id'] = institution.id
@@ -25,6 +33,8 @@ def to_dict(institution):
     dict_format['passwd'] = institution.passwd
     dict_format['types'] = institution.types.split(',')
     dict_format['shelter'] = institution.shelter
+    dict_format['status'] = status_enum[str(institution.status)]
+    dict_format['created_at'] = institution.created_at.strftime('%Y-%m-%d %H:%M:%S')
     return dict_format
 
 @blueprint.route('/institution', methods=['POST', 'OPTIONS'])
@@ -52,7 +62,7 @@ def post_institution():
     shelter = int(body['shelter'])
 
     institution = institution_model.create_institution(name=name, address=address, email=email, passwd=passwd,
-                                                       types=types, shelter=shelter)
+                                                       types=types, shelter=shelter, status=0)
     result = to_dict(institution)
 
     return jsonify(result), 200
@@ -78,6 +88,41 @@ def delete_institution(institution_id):
     institution = institution_model.get_institution(institution_id)
     if institution:
         institution_model.delete_institution(institution.id)
+        result = to_dict(institution)
+        return jsonify(result), 200
+    else:
+        endpoints_exception(404, "REGION_NOT_FOUND")
+
+
+@blueprint.route('/institution/<institution_id>', methods=['PUT', 'OPTIONS'])
+def put_institution(institution_id):
+    """
+    institutions put must follow:
+    {
+      "name": "string",
+      "address": "string"
+      "email" string,
+      "passwd": string,
+      "types": string,
+      "shelter": int
+      "status": "string"
+    }
+    :return: a region entity
+    """
+    result = {}
+
+    body = request.json
+    name = body.get("name", None)
+    address = body.get("address", None)
+    email = body.get("email", None)
+    passwd = body.get("passwd", None)
+    types = body.get("types", None)
+    shelter = body.get("shelter", None)
+    status = body.get("status", None)
+
+    institution = institution_model.get_institution(institution_id)
+    if institution:
+        institution_model.update_institution(institution, name, address, email, passwd, types, shelter, status)
         result = to_dict(institution)
         return jsonify(result), 200
     else:
--- api/models/institution.py
+++ api/models/institution.py
@@ -6,13 +6,29 @@ A model for the region entity.
 """
 from services.database import get_db_instance
 import logging
+from datetime import datetime
+
 
 _logger = logging.getLogger(__name__)
 table_name = 'institution'
 
 
+status_enum = {
+    "0": 'NEW',
+    "1": 'PENDING',
+    "2": 'ACTIVE',
+    "3": 'DENIED',
+}
+
+status_enum_reverse = {
+    'NEW': 0,
+    'PENDING': 1,
+    'ACTIVE': 2,
+    'DENIED': 3,
+}
+
 class Institution():
-    def __init__(self, id, address, name, email, passwd, types, shelter):
+    def __init__(self, id, address, name, email, passwd, types, shelter, status, created_at):
         self.id = id
         self.address = address
         self.name = name
@@ -20,22 +36,27 @@ class Institution():
         self.passwd = passwd
         self.types = types
         self.shelter = shelter
+        self.status = status
+        self.created_at = created_at
 
     def __repr__(self):
         return "<Institution(id='%s', name=%s)>" % (self.id, self.name)
 
 
-def create_institution(address, name, email, passwd, types, shelter):
+def create_institution(address, name, email, passwd, types, shelter, status):
     db = get_db_instance()
     institution = None
     try:
         with db.cursor() as cursor:
             # insert record
-            sql = "INSERT INTO {} (ADDRESS, NAME, EMAIL, PASSWD, TYPES, SHELTER) VALUES ('{}', '{}', '{}','{}','{}',{})"
-            cursor.execute(sql.format(table_name, address, name, email, passwd, types, shelter))
+            sql = "INSERT INTO {} (ADDRESS, NAME, EMAIL, PASSWD, TYPES, SHELTER, STATUS) VALUES " \
+                  "('{}', '{}', '{}','{}','{}',{},{})"
+            cursor.execute(sql.format(table_name, address, name, email, passwd, types, shelter, status))
             created_id = db.insert_id()
             cursor.execute('commit')
-            institution = Institution(id=created_id, address=address, name=name, email=email, passwd=passwd, types=types, shelter=shelter)
+            institution = Institution(id=created_id, address=address, name=name, email=email, passwd=passwd,
+                                      types=types, shelter=shelter, status=status,
+                                      created_at=datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
             return institution
     finally:
         db.close()
@@ -46,7 +67,7 @@ def get_institutions():
     try:
         with db.cursor() as cursor:
             # Read all records
-            sql = "select * from {}"
+            sql = "select * from {} order by STATUS,CREATED_AT"
             cursor.execute(sql.format(table_name))
             result = cursor.fetchall()
             if result:
@@ -58,7 +79,9 @@ def get_institutions():
                         'email': row[3],
                         'password': row[4],
                         'types': row[5].split(','),
-                        'shelter': row[6]
+                        'shelter': row[6],
+                        'status': status_enum[str(row[7])],
+                        'created_at': row[8].strftime('%Y-%m-%d %H:%M:%S')
                     })
                 return data
             else:
@@ -77,7 +100,8 @@ def get_institution(iid):
             result = cursor.fetchone()
             if result:
                 institution = Institution(id=result[0], address=result[1], name=result[2], email=result[3],
-                                          passwd=result[4], types=result[5], shelter=result[6])
+                                          passwd=result[4], types=result[5], shelter=result[6], status=result[7],
+                                          created_at=result[8])
                 return institution
             else:
                 return None
@@ -93,5 +117,43 @@ def delete_institution(iid):
             cursor.execute(sql.format(table_name, iid))
             cursor.execute('commit')
             return True
+    finally:
+        db.close()
+
+def update_institution(institution, name, address, email, passwd, types, shelter, status):
+    from utils.password_utils import convert_md5
+    db = get_db_instance()
+    try:
+        with db.cursor() as cursor:
+            # update a single record
+            sql = "update {} set ".format(table_name)
+            if name:
+                sql += "NAME = '{}', ".format(name)
+                institution.name = name
+            if address:
+                sql += "ADDRESS = '{}', ".format(address)
+                institution.address = address
+            if email:
+                sql += "EMAIL = '{}', ".format(email)
+                institution.email = email
+            if passwd:
+                sql += "PASSWD = '{}', ".format(convert_md5(passwd))
+                institution.passwd = convert_md5(passwd)
+            if types:
+                sql += "TYPES = '{}', ".format(types)
+                institution.types = type
+            if shelter:
+                sql += "SHELTER = {}, ".format(shelter)
+                institution.shelter = shelter
+            if status:
+                sql += "STATUS = {}, ".format(status_enum_reverse[status])
+                institution.status = status_enum_reverse[status]
+            sql += "UPDATED_AT = '{}', ".format(datetime.now())
+            sql = sql[:-2] + " "
+            sql += "where ID_INSTITUTION = {}".format(institution.id)
+
+            cursor.execute(sql)
+            cursor.execute('commit')
+            return institution
     finally:
         db.close()
